d0bb8cdcbf3699c38bb993a25a958b0fb7f872af
Ahmet Arda Öz <70638681+AhmetArdaOz@users.noreply.github.com>
2024-06-21 22:29:49 +0300

    completed

--- portfolio_app/src/App.jsx
+++ portfolio_app/src/App.jsx
@@ -24,14 +24,9 @@ const App = () => {
         <Parallax type="portfolio" />
       </section>
       <Portfolio />
-      {/* <section>Portfolio1</section>
-      <section>Portfolio2</section>
-      <section>Portfolio3</section> */}
       <section id="Contact">
         <Contact />
       </section>
-      {/* <Test />
-      <Test /> */}
     </div>
   );
 };
