5f7f3e9f0dd4224d764a21361f5798157f66afb1
Ahmet Arda Öz <70638681+AhmetArdaOz@users.noreply.github.com>
2024-07-01 22:00:31 +0300

    Update Contact.jsx

--- portfolio_app/src/components/contact/Contact.jsx
+++ portfolio_app/src/components/contact/Contact.jsx
@@ -60,7 +60,7 @@ const Contact = () => {
         </motion.div>
         <motion.div className="item" variants={variants}>
           <h2>Address</h2>
-          <span>Ankara/Mamak</span>
+          <span>Ankara/Turkey</span>
         </motion.div>
         <motion.div className="item" variants={variants}>
           <h2>Phone</h2>
